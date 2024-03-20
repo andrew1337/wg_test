9162bd4532d218f8d4780a9b65d5bf1633f2dd82
andrew1337 <andrey.h@cattle-care.com>
2024-03-20 03:15:24 +0100

    few smaller classes is better than one

--- app/game/game_room.py
+++ app/game/game_room.py
@@ -14,14 +14,33 @@ class PlayerResult(Enum):
     LOSE = auto()
     DRAW = auto()
 
-class GameState(Enum): # todo: move staet here adn to refacror
-    timer: Optional[Timer]
-    is_ended: asyncio.Event
-    result: Optional[GameResult]
+
+class UserInteractions:
+    def __init__(self, game_room: "GameRoom"):
+        self.game_room = game_room
+
+    async def handle_connection(self, ws: WebSocketServerProtocol, message: str):
+        try:
+            await self.handle_message(ws, message)
+        except ConnectionClosed:
+            self.handle_disconnection(ws)
+            raise
+
+    async def handle_message(self, ws: WebSocketServerProtocol, message: dict):
+        choice_str = message.get("choice", None)
+        player = self.game_room.ws_player_mapping.get(ws, None)
+        if choice_str and player:
+            self.game_room.handle_choice(player, choice_str)
+
+    def handle_disconnection(self, ws: WebSocketServerProtocol):
+        player = self.game_room.ws_player_mapping[ws]
+        self.game_room.game.disconnected.add(player)
+
 
 class GameRoom:
     def __init__(self, capacity: int, scoreboard: UsersScoreboard):
         self.players: Dict[str, WebSocketServerProtocol] = {}
+        self.ws_player_mapping: Dict[WebSocketServerProtocol, str] = {}
         if capacity < 2:
             raise ValueError(f"Room capacity should be >= 2, got: {capacity}")
         self.capacity = capacity
@@ -32,65 +51,64 @@ class GameRoom:
         self.game_class = Game
         self.game: Optional[Game] = None
         self.game_is_ended = asyncio.Event()
-        self.game_result: Optional[GameResult] = None
         self.scoreboard: UsersScoreboard = scoreboard
+        self.user_interactions = UserInteractions(self)
 
-    def _get_player_by_ws(self, ws: WebSocketServerProtocol) -> Optional[str]:
-        for player, w in self.players.items():
-            if w == ws:
-                return player
-        return None
+    def handle_choice(self, player: str, msg: str):
+        try:
+            move = Move(msg.strip().lower())
+        except ValueError:
+            return
+        is_accepted = self.game.do_move(player=player, choice=move)
+        if not is_accepted:
+            return
+        if self.game.is_all_connected_players_are_made_a_move():
+            self.game_is_ended.set()
 
     async def add_player(self, player_name: str, ws: WebSocketServerProtocol):
         self.players[player_name] = ws
+        self.ws_player_mapping[ws] = player_name
         await self.notify_all({"ping": f"Player {player_name} joined the game."})
         if len(self.players) == self.capacity:
             await self.start_game()
 
     async def start_game(self):
-        if len(self.players) != self.capacity:
-            print("Problem with start_game()")
+        if len(self.players) < 2:
+            raise RuntimeError("Not enough players")
         self.game_is_ended.clear()
-        self.game_result = None
         players_names = list(self.players.keys())
         self.game = self.game_class(players_names)
-
         await self.notify_all({"game_state": "start", "players": players_names})
         await self.start_timer()
-        await asyncio.sleep(0.5)
-
-    async def _last_tick(self):
-        await asyncio.sleep(1)
-        self.game_is_ended.set()
+        await self.game_is_ended.wait()
         self.timer.stop()
-        self.game_result = self.game.stop_game()
-        await self.notify_all({"game_state": "countdown", "countdown": 0})
-        await self.notify_all({"game_state": "stop", "stop": "countdown"})
-        await self.notify_results()
+        game_result = self.game.stop_game()
+        await self.manage_results(game_result)
+        if game_result == GameResult.TIE:
+            await self.do_restart()
 
     async def start_timer(self):
         self.timer = self.timer_class(self.timer_inintial_seconds)
         async for remaining_time in self.timer.start():
-            if self.game.is_all_players_made_a_move() or self.game_is_ended.is_set():
-                self.game_is_ended.set()
-                self.timer.stop()
+            if self.game_is_ended.is_set():
                 break
-            await self.notify_all(
-                {"game_state": "countdown", "countdown": remaining_time, "players": list(self.players.keys())}
-            )
+            message = {
+                "game_state": "countdown",
+                "countdown": remaining_time,
+                "players": list(self.players.keys()),
+            }
+            await self.notify_all(message)
             if remaining_time == 1:
                 await self._last_tick()
 
-    async def handle_connection(self, ws: WebSocketServerProtocol, message: str):
-        try:
-            await self.handle_message(ws, message)
-        except ConnectionClosed:
-            self.handle_disconnection(ws)
-            raise
+    async def _last_tick(self):
+        await asyncio.sleep(1)
+        await self.notify_all({"game_state": "countdown", "countdown": 0})
+        self.game_is_ended.set()
 
     async def do_restart(self):
-        draw_timer = self.timer_class(3)
-        async for i in draw_timer.start():
+        restart_timer = self.timer_class(3)
+        async for i in restart_timer.start():
             await self.notify_all(
                 {
                     "game_state": "result",
@@ -100,50 +118,22 @@ class GameRoom:
             )
         await self.start_game()
 
-    async def handle_message(self, ws: WebSocketServerProtocol, message: dict):
-        choice_str = message.get("choice", None)
-        if choice_str:
-            await self.handle_choice(ws, choice_str)
-        if self.game.is_all_players_made_a_move():
-            self.game_is_ended.set()
-            self.timer.stop()
-            self.game_result = self.game.stop_game()
-            await self.notify_all(
-                {"game_state": "stop", "stop": "all_players_made_a_move"}
-            )
-            await self.notify_results()
-
-    async def handle_choice(self, ws: WebSocketServerProtocol, msg: str):
-        try:
-            move = Move(msg.strip().lower())
-            is_accepted = self.game.handle_choice(
-                player=self._get_player_by_ws(ws), choice=move
-            )
-            await ws.send_json({"ping": f"Choice made: {is_accepted}"})
-        except ValueError:
-            await ws.send_json({"ping": f"Invalid choice {msg=}"})
-
-    def handle_disconnection(self, ws: WebSocketServerProtocol):
-        player = self._get_player_by_ws(ws)
-        print(f"disconnected: {player=}")
-        self.game.disconnected.add(player)
-
-    async def notify_results(self):
-        if self.game_result == GameResult.TIE:
+    async def manage_results(self, result: GameResult):
+        if result == GameResult.TIE:
             await self.notify_all(
                 {"game_state": "result", "result": PlayerResult.DRAW.name}
             )
-            # await self.do_restart()
             return
         for player, ws in self.players.items():
+            result = (
+                PlayerResult.WIN.name
+                if player in self.game.result.winners
+                else PlayerResult.LOSE.name
+            )
             await ws.send_json(
                 {
                     "game_state": "result",
-                    "result": (
-                        PlayerResult.WIN.name
-                        if player in self.game.result.winners
-                        else PlayerResult.LOSE.name
-                    ),
+                    "result": result,
                 }
             )
         self.write_down_results()
@@ -159,4 +149,4 @@ class GameRoom:
             try:
                 await ws.send_json(message)
             except RuntimeError:
-                print(f"Skip sending message to removed {ws=}")
+                pass
